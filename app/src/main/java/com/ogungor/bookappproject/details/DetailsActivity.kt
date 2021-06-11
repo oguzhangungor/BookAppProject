7e194e4aad5a235885111acb1af0ef69a6526ea8
Oğuzhan Güngör <69319236+oguzhangungor@users.noreply.github.com>
2021-06-11 09:39:39 +0300

    Details layout design

--- app/src/main/java/com/ogungor/bookappproject/details/DetailsActivity.kt
+++ app/src/main/java/com/ogungor/bookappproject/details/DetailsActivity.kt
@@ -1,6 +1,13 @@
 package com.ogungor.bookappproject.details
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ogungor.bookappproject.R
 
 class DetailsActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.details_layout)
+    }
 }
--- app/src/main/java/com/ogungor/bookappproject/main/MainActivity.kt
+++ app/src/main/java/com/ogungor/bookappproject/main/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.ogungor.bookappproject.main
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.OrientationHelper
+import androidx.viewpager.widget.ViewPager
 import com.ogungor.bookappproject.R
+import com.ogungor.bookappproject.details.DetailsActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.populer_item.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,16 +23,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var myModelList: ArrayList<MyModel>
 
     private lateinit var myAdapter: MyAdapter
+    private lateinit var viewPager: ViewPager
+
+    private lateinit var image:ImageView
 
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        viewPager = findViewById(R.id.viewPager)
+        image=findViewById(R.id.ellipse_white)
 
         loadCards()
-
+        onClickDetails()
         popularPosts = ArrayList()
         recommendedPosts = ArrayList()
 
@@ -68,15 +78,25 @@ class MainActivity : AppCompatActivity() {
             LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false)
         recyclerView_recommen.adapter = PostsAdapter(recommendedPosts)
 
+
     }
 
     private fun loadCards() {
         myModelList = ArrayList()
 
         myModelList.add(MyModel(R.drawable.oceans))
+        myModelList.add(MyModel(R.drawable.orange))
 
         myAdapter = MyAdapter(this, myModelList)
 
         viewPager.adapter = myAdapter
+    }
+
+    private fun onClickDetails() {
+        image.setOnClickListener {
+            var intent = Intent(this, DetailsActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
